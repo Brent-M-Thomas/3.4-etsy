bbd00f5fb725f83af45157a80532fc2dfd6db6f7
Brent Thomas <brentmt24@gmail.com>
2015-08-08 17:15:43 -0500

    broken_etsy

--- dist/app.js
+++ dist/app.js
@@ -1,36 +1,18 @@
-var timeEl = document.getElementsByClassName('time')[0];
-var buttonEl = document.getElementsByTagName('button')[0];
+(function() {
 
-var interval;
+var mainContentEl = document.querySelector('.main-content');
 
-function padZeros (num) {
-    if (num < 10) {
-        return '0' + num;
-    }
-    return num;
-}
+var templateFromQuery = function(query) {
+  var el = document.querySelector(query);
+  return Handlebars.compile(el.innerHTML);
+};
 
-function buttonHasBeenClicked () {
-    window.clearInterval(interval);
+var template = templateFromQuery('#item');
 
-    timeEl.innerHTML = '01:00';
-    buttonEl.innerHTML = 'Reset';
-    var finishTime = new Date().valueOf() + 1000 * 1 * 60;
+loadEtsy.data.forEach(function(item) {
+    var addItem = template(item);
 
-    var renderTimeSince = function () {
-        var currentTime = new Date().valueOf();
-        var timeDiff = finishTime - currentTime;
-        var secSinceStart =  parseInt(timeDiff / 1000) % 60;
-        var minSinceStart =  parseInt(timeDiff / 60000);
+    mainContentEl.innerHTML += addItem;
+  });
 
-        timeEl.innerHTML = padZeros(minSinceStart) + ':' + padZeros(secSinceStart);
-
-        if (secSinceStart <= 0 && minSinceStart <= 0) {
-            window.clearInterval(interval);
-        }
-    };
-
-    interval = window.setInterval(renderTimeSince, 1000);
-}
-
-buttonEl.onclick = buttonHasBeenClicked;
+})();
