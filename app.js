b4836c193507c35ecb7c74e0c3fce509dadf7d5d
Brent Thomas <brentmt24@gmail.com>
2015-09-21 13:48:52 -0500

    added links

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
 (function() {
 
-var mainContentEl = document.querySelector('.main-content');
+// var mainContentEl = document.querySelector('.main-template');
 
-var templateFromQuery = function(query) {
-  var el = document.querySelector(query);
-  return Handlebars.compile(el.innerHTML);
-};
+// var templateFromQuery = function(query) {
+//   var el = document.querySelector(query);
+//   return Handlebars.compile(el.innerHTML);
+// };
 
-var template = templateFromQuery('#item');
+// var template = templateFromQuery('.item');
 
-loadEtsy.data.forEach(function(item) {
-    var addItem = template(item);
+// loadEtsy('abstract painting', function(query) {
+//     var addItem = template(query);
 
-    mainContentEl.innerHTML += addItem;
-  });
+//     mainContentEl.innerHTML += addItem;
+//   });
+
+var contentEl = document.querySelector('.main-content');
+
+function registerByQuery(querySelector) {
+  var templateString = document.querySelector(querySelector).innerHTML;
+  templateString = templateString.replace('&gt;', '>');
+  return Handlebars.compile(templateString);
+}
+
+function registerPartialByQuery(name) {
+  var templateString = document.querySelector('#' + name + '-partial').innerHTML;
+  return Handlebars.registerPartial(name, templateString);
+}
+
+registerPartialByQuery('item');
+
+var handlebarsTemplate = registerByQuery('#main-template');
+
+loadEtsy('abstract painting', function(data) {
+  var allItems = '';
+
+  allItems = handlebarsTemplate(data);
+
+  contentEl.innerHTML = allItems;
+});
 
 })();
--- load-etsy.js
+++ load-etsy.js
@@ -11,7 +11,7 @@ var loadEtsy = function(query, onload, immediate) {
     url: url,
     jsonp: 'callback',
     dataType: 'jsonp',
-    success: onload
+    success: onload,
   });
 
   if (immediate) {
@@ -26,4 +26,4 @@ var loadEtsy = function(query, onload, immediate) {
 
 };
 
-loadEtsy('abstract painting', onload);
+// loadEtsy('abstract painting', onload);
